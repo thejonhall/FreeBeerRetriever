f62722f8b893f1f189602ef1573f583ba92ccaf7
Jon Hall <jon.hall@asynchrony.com>
2016-02-10 22:41:54 -0600

    Initial Commit

---
+++ FreeBeerRetriever/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FreeBeerRetriever
+//
+//  Created by jon.hall on 2/10/16.
+//  Copyright © 2016 jon.hall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
