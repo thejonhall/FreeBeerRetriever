7706e798f1cca5d31c2045ba83f1f04a8b216c16
Jon Hall <jon.hall@asynchrony.com>
2016-05-12 21:19:35 -0500

    Updated Kanna pod. -Jon

--- FreeBeerRetriever/ViewController.swift
+++ FreeBeerRetriever/ViewController.swift
@@ -1,43 +1,49 @@
 import UIKit
 import Kanna
+import FreeBeerRetriever
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let request = NSMutableURLRequest(URL: NSURL(string: "http://www.saucerknurd.com/glassnite/quiz/")!)
-        request.HTTPMethod = "POST"
-        let postString = "email=thejonhall@gmail.com&UFO=00191&homestore=39"
-        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
-            guard error == nil && data != nil else {
-                print("error=\(error)")
-                return
-            }
-            
-            if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 200 {
-                print("statusCode should be 200, but is \(httpStatus.statusCode)")
-                print("response = \(response)")
-            }
-            
-            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
-            print("responseString = \(responseString)")
-            
-            self.parseQuizAnswersFromSourceString(responseString!)
-            
-        }
-        task.resume()
+//        let request = NSMutableURLRequest(URL: NSURL(string: "http://www.saucerknurd.com/glassnite/quiz/")!)
+//        request.HTTPMethod = "POST"
+//        let postString = "email=thejonhall@gmail.com&UFO=00191&homestore=39"
+//        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+//        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+//            guard error == nil && data != nil else {
+//                print("error=\(error)")
+//                return
+//            }
+//            
+//            if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 200 {
+//                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+//                print("response = \(response)")
+//            }
+//            
+//            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+//            print("responseString = \(responseString)")
+//            
+//            self.parseQuizAnswersFromSourceString(responseString!)
+//            
+//        }
+//        task.resume()
         
 //        Read from file
         
-//        do {
-//            let htmlPath = NSString(string:"/Users/jon.hall/Desktop/captainKeith.txt").stringByExpandingTildeInPath
-//            let fileContent = try NSString(contentsOfFile: htmlPath, encoding: NSUTF8StringEncoding)
-//            parseQuizAnswersFromSourceString(fileContent as String)
-//        } catch {
-//            print(error)
-//        }
+        let model : BeerRetrieverModel = BeerRetrieverModel()
+        
+        do {
+            let htmlPath = NSString(string:"/Users/jon.hall/Documents/captainKeith.txt").stringByExpandingTildeInPath
+            let fileContent = try NSString(contentsOfFile: htmlPath, encoding: NSUTF8StringEncoding)
+            let answeredQuestions = model.parseQuizAnswersFromSourceString(fileContent as String)
+            for answeredQuestion in answeredQuestions {
+                print (answeredQuestion.formattedAnswer)
+            }
+        } catch {
+            print(error)
+        }
     
 
     }
